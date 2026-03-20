e74bebc61605a8bd3a0456b916324731165e3f72
whisp-r <188515789+whisp-r@users.noreply.github.com>
2026-03-20 11:59:53 +0100

    fixed python versioning

--- shell.nix
+++ shell.nix
@@ -1,17 +1,23 @@
-{
-  pkgs ? import <nixpkgs> { },
-}:
-
+let
+  oldpkgs =
+    import
+      (fetchTarball "https://github.com/NixOS/nixpkgs/archive/06278c77b5d162e62df170fec307e83f1812d94b.tar.gz")
+      { };
+  pkgs = import <nixpkgs> { };
+in
 pkgs.mkShell {
-  packages = with pkgs; [
-    nixfmt
-    nixd
-    nil
+  packages =
+    with pkgs;
+    [
+      nixfmt
+      nixd
+      nil
 
-    ruff
-    ty
-    uv
-    python313
-    python313Packages.debugpy
-  ];
+      ruff
+      ty
+      uv
+    ]
+    ++ [
+      oldpkgs.python38
+    ];
 }
