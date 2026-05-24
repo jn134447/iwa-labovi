4f4b64c28cb0ad0d4858e7ebceba124d36f64857
whisp-r <188515789+whisp-r@users.noreply.github.com>
2026-05-24 13:12:01 +0200

    view, link, search done

--- seminar/seminar/views.py
+++ seminar/seminar/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+
+from .models import Book
+from django.db import models
+
+def book_list(request):
+    books = Book.objects.all()
+    query = request.GET.get("q")
+    author = request.GET.get("author")
+
+    if query:
+        books = books.filter(title__icontains=query)
+    if author:
+        books = books.filter(
+            models.Q(authors__first_name__icontains=author) |
+            models.Q(authors__last_name__icontains=author)
+        )
+
+    return render(request, "seminar/book_list.html", {"books": books})
+
+def book_detail(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    return render(request, "seminar/book_detail.html", {"book": book})
+
